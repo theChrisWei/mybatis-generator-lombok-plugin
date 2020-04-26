12f3701a9e5288a2749ed99431e0ebd666c3ef17
Vi <weikang0827@gmail.com>
2020-04-27 00:10:59 +0800

    - source compiler version 6 -> 8 - plugin version 1.3.2 -> 1.4.0 - code improvement

--- src/main/java/com/chrm/mybatis/generator/plugins/CommentPlugin.java
+++ src/main/java/com/chrm/mybatis/generator/plugins/CommentPlugin.java
@@ -14,19 +14,21 @@ import java.util.*;
 
 /**
  *
- *  @Author: GuiRunning 郭贵荣
+ *  @Author: GuiRunning 郭贵荣      updated by Wei
  *
  *  @Description: 自定义注释生成
  *
- *  @Date: 2018/7/14 0:57
+ *  @Date: 2020/4/26
  *
  */
 public class CommentPlugin extends PluginAdapter {
 
+    @Override
     public boolean validate(List<String> warnings) {
         return true;
     }
 
+    @Override
     public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
         topLevelClass.getJavaDocLines().clear();
         topLevelClass.addJavaDocLine("/**");
@@ -35,15 +37,18 @@ public class CommentPlugin extends PluginAdapter {
         return true;
     }
 
+    @Override
     public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
         this.comment(field, introspectedTable, introspectedColumn);
         return true;
     }
 
+    @Override
     public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
         return true;
     }
 
+    @Override
     public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
         return true;
     }
@@ -63,18 +68,20 @@ public class CommentPlugin extends PluginAdapter {
         element.addJavaDocLine(" */");
     }
 
+    @Override
     public boolean sqlMapResultMapWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
         this.commentResultMap(element, introspectedTable);
         return true;
     }
 
+    @Override
     public boolean sqlMapResultMapWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
         this.commentResultMap(element, introspectedTable);
         return true;
     }
 
     void commentResultMap(XmlElement element, IntrospectedTable introspectedTable) {
-        List<Element> es = element.getElements();
+        List<VisitableElement> es = element.getElements();
         if (!es.isEmpty()) {
             String alias = introspectedTable.getTableConfiguration().getAlias();
             int aliasLen = -1;
@@ -82,12 +89,12 @@ public class CommentPlugin extends PluginAdapter {
                 aliasLen = alias.length() + 1;
             }
 
-            Iterator<Element> it = es.iterator();
+            Iterator<VisitableElement> it = es.iterator();
             HashMap map = new HashMap();
 
             while(true) {
                 while(it.hasNext()) {
-                    Element e = (Element)it.next();
+                    VisitableElement e = (VisitableElement)it.next();
                     if (e instanceof TextElement) {
                         it.remove();
                     } else {
@@ -95,11 +102,11 @@ public class CommentPlugin extends PluginAdapter {
                         List<Attribute> as = el.getAttributes();
                         if (!as.isEmpty()) {
                             String col = null;
-                            Iterator i$ = as.iterator();
+                            Iterator iterator = as.iterator();
 
-                            while(i$.hasNext()) {
-                                Attribute a = (Attribute)i$.next();
-                                if (a.getName().equalsIgnoreCase("column")) {
+                            while(iterator.hasNext()) {
+                                Attribute a = (Attribute)iterator.next();
+                                if ("column".equalsIgnoreCase(a.getName())) {
                                     col = a.getValue();
                                     break;
                                 }
@@ -110,7 +117,7 @@ public class CommentPlugin extends PluginAdapter {
                                     col = col.substring(aliasLen);
                                 }
 
-                                IntrospectedColumn ic = introspectedTable.getColumn(col);
+                                IntrospectedColumn ic = introspectedTable.getColumn(col).get();
                                 if (ic != null) {
                                     StringBuilder sb = new StringBuilder();
                                     if (ic.getRemarks() != null && ic.getRemarks().length() > 1) {
@@ -129,13 +136,13 @@ public class CommentPlugin extends PluginAdapter {
                     return;
                 }
 
-                Set<Element> set = map.keySet();
-                Iterator i$ = set.iterator();
+                Set<VisitableElement> set = map.keySet();
+                Iterator iterator = set.iterator();
 
-                while(i$.hasNext()) {
-                    Element e = (Element)i$.next();
+                while(iterator.hasNext()) {
+                    VisitableElement e = (VisitableElement)iterator.next();
                     int id = es.indexOf(e);
-                    es.add(id, (Element) map.get(e));
+                    es.add(id, (VisitableElement) map.get(e));
                     es.add(id, new TextElement(""));
                 }
 
@@ -144,26 +151,31 @@ public class CommentPlugin extends PluginAdapter {
         }
     }
 
+    @Override
     public boolean sqlMapInsertElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
         this.removeAttribute(element.getAttributes(), "parameterType");
         return true;
     }
 
+    @Override
     public boolean sqlMapInsertSelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
         this.removeAttribute(element.getAttributes(), "parameterType");
         return true;
     }
 
+    @Override
     public boolean sqlMapUpdateByPrimaryKeySelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
         this.removeAttribute(element.getAttributes(), "parameterType");
         return true;
     }
 
+    @Override
     public boolean sqlMapUpdateByPrimaryKeyWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
         this.removeAttribute(element.getAttributes(), "parameterType");
         return true;
     }
 
+    @Override
     public boolean sqlMapUpdateByPrimaryKeyWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
         this.removeAttribute(element.getAttributes(), "parameterType");
         return true;
@@ -186,9 +198,10 @@ public class CommentPlugin extends PluginAdapter {
         }
     }
 
+    @Override
     public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
         document.getRootElement().addElement(new TextElement(""));
-        document.getRootElement().addElement(new TextElement("<!-- ### 以上代码由MBG + CommentPlugin自动生成, 生成时间: " + (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(new Date()) + " ### -->\n\n\n"));
+        document.getRootElement().addElement(new TextElement("<!-- ### Generated By MBG + CommentPlugin, generated time: " + (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(new Date()) + " ### -->\n\n\n"));
         document.getRootElement().addElement(new TextElement("<!-- Your codes goes here!!! -->"));
         document.getRootElement().addElement(new TextElement(""));
         return true;
